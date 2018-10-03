a8a55bfa9f800b11b89a846db5faaa35d33fbe06
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-03 10:57:58 -0700

    requiring more packages, sets app = express()

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("bodyParser");
-
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 const axios = require("axios");
 const cheerio = require("cheerio");
+
+
+app
