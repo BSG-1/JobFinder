6b78cc0a7eb69bfe7dd3439892f0b44440824905
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-03 11:06:10 -0700

    creates simple server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const bodyParser = require("bodyParser");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const axios = require("axios");
-const cheerio = require("cheerio");
+app.get('/', function (req, res) {
+    res.send("Job Finder");
+});
 
-
-app
+app.listen(PORT, () => {
+    console.log("Server listening on port", PORT);
+})
