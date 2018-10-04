e7c9843c07a3128578bac2667aa4acb9f70c4ac4
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-04 14:21:42 -0700

    console test for 'scraping'

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
+    console.log("scraping!");
     res.render("index", { title: "JobFinder" })
 });
 
