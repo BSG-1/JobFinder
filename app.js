5145f04525950fd3959b7b0b0f8fbec492447c27
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-04 12:54:33 -0700

    adds handlebars

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
+
+const handlebars = require("express-handlebars");
+
+app.engine('.hbs', handlebars({ extname: '.hbs' }));
