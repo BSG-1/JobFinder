db08ed5edf2b90c0674c57215736099c24c4d957
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-04 12:59:38 -0700

    initializes basic server, listening successfully

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ const handlebars = require("express-handlebars");
 app.engine('.hbs', handlebars({ extname: '.hbs' }));
 
 app.set("PORT: ", PORT);
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
+
+app.get('/', function (req, res) {
+    res.render("index", { title: "JobFinder" })
+});
+
+app.listen(app.get('PORT'), function () {
+    console.log("Listening on http://localhost:" + PORT);
+});
