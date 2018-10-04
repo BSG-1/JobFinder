75cf83b0c59c017254d012c42edab36743495b8a
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-04 12:41:32 -0700

    initializes package.json, updates axios, express, handlebars

--- server.js
+++ server.js
@@ -17,3 +17,14 @@ app.listen(PORT, () => {
 var request = require("request");
 var cheerio = require("cheerio");
 var url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 94526;
+
+request(url, function (error, response, body) {
+    if (!error) {
+        var $ = cheerio.load(body),
+            temperature = $("[data-variable='temperature'].wx-value").html();
+
+        console.log("It's " + temperature + " degrees Fahrenheit.");
+    } else {
+        console.log("We've encountered an error: " + error);
+    }
+});
